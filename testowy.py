8447129a3e566403d44e58b23c19d123db86899c
kacper <kacper@gmail.com>
2025-10-07 14:00:35 +0200

    zbior zmiennych

--- testowy.py
+++ testowy.py
@@ -1,3 +1,1 @@
-print("hello world")
-print("cześć")
-print("witaj")
+
---
+++ zmienne.py
@@ -0,0 +1,36 @@
+#co tutaj sie dzzieje?
+#1)   rezerwujemy kawalek pamieci
+#2)   umieszczamy w tym kawalku pamieci liczbe 45
+#3)   nadajemy temu kawalkowi pamieci liczba calkowita
+
+liczba_calkowita = 45
+print(type(liczba_calkowita))   #sprawdzamy i wypisujemy typ zmiennej
+
+liczba_z_przecinkiem = 2.65
+print(type(liczba_z_przecinkiem))
+
+'''
+tu sa pozostale
+typy zmiennych
+ponieważ są świetne
+'''
+
+napis = "informatyka"
+print(type(napis))
+
+wartosc_logiczna = True
+print(type(wartosc_logiczna))
+
+lista = [5, 'x', [5, 7], [[4, 6], 9], True, 'wsad']
+print(type(lista))
+
+zbior = {5, 5, 5, 6, 1, 9}
+print(zbior)
+
+slownik = {'dekadentyzm': 'prad filozoficzny gloszacy ze nic nie ma sensu', 5: 'piec'}  #haslo: wyjasnienie
+print(type(slownik))
+
+
+
+
+
