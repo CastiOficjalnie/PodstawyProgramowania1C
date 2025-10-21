b701be250e2fc486d94827a713b5cba8927a4b4c
kacper <kacper@gmail.com>
2025-10-21 13:42:41 +0200

    logika

---
+++ operatory_logiczne.py
@@ -0,0 +1,44 @@
+#symbole logiczne
+
+#koniunckja
+print(2 == 3 and 3 > 1)
+
+#alternatywa
+login = 'qwerty'
+haslo = 'uiop'
+print(login == 'admin' or haslo == 'uiop')
+
+#zzaprzeczenie
+print(not(login == 'admin' or haslo == 'uiop'))
+
+#alternatywa wykluczajaca
+fryjer = True
+murarz = True
+print(fryjer == False ^ murarz == True)
+
+#priorytety operatorow logicznych
+print(2 == 3 and 3 < 1 or 2 < 6)
+
+#operatory standardowe
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+
+#potegowanie
+print(2 ** 3)
+
+#pierwiastkowanie
+print(36 ** 0.5)
+print(125 ** (1/3))
+
+#dzielenie calkowite (div)
+print(12 // 5)
+#
+#
+#
+#
+#
+
+
+
--- wejscie.py
+++ wejscie.py
@@ -1,19 +1,7 @@
-#1. wprowadzanie danych przez uzytkownika do programu
-from zmienne import liczba_z_przecinkiem
+#koniunkcja
+print(2 == 3 and 3 > 1)
 
-#   1)utworzenie zmiennej
-#   2)na ekranie pojawia sie komunikat z inputa
-#   3)program sie zatrzymuje i czeka na podanie i zatwierdzenie(ENTER) dancyh
-#   4)po wpisaniu i zatwierdzeniu to co wpiszemy trafia jako string to zmiennej
-#   5)program wykonuje sie dalej
-dane = input("podaj swoje imie ")
-
-liczba = int(input('podaj liczbe '))
-print(type(liczba))
-
-liczba_z_przecinkie = float(input('podaj liczbe z przecinkiem '))
-print(type(liczba_z_przecinkie))
-
-cos = list(input('podaj cos'))
-print(cos)
-print(type(cos))
+#alternatywa
+login = "qwerty"
+haslo = 'viop'
+print(login == 'admin' or haslo == "viop")
