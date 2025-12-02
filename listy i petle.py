9d55545bf31bb032072bb8ff79ff3233db21b3af
kacper <kacper@gmail.com>
2025-12-02 14:29:28 +0100

    lekcja

--- listy i petle.py
+++ listy i petle.py
@@ -1,3 +1,4 @@
+from math import inf
 # jak nie powtarzac wiele razy tego samego
 
 # x = float(input('podaj liczbe'))
@@ -5,7 +6,7 @@
 # z = float(input('podaj liczbe'))
 # k = float(input('podaj liczbe'))
 # l = float(input('podaj liczbe'))
-#
+#inf = bardzo duża liczba
 # suma = (x + y + z + k + l)
 
 # print(suma)'''
@@ -20,7 +21,7 @@
 # print(suma)
 
 # 1. listy
-lista = ['qwerty', 56, [6, 7], 4.56, [[5,8], 1]]
+'''lista = ['qwerty', 56, [6, 7], 4.56, [[5,8], 1]]
 print(lista[2][1])
 print(lista[4][0][1])
 
@@ -54,9 +55,9 @@ for i in lista4:
 # for i in range(10):
 #     print(i)
 
-'''lista5 = [5]
+''lista5 = [5]
 lista.append(0)
-print(lista5)'''
+print(lista5)
 
 # lista = [0]
 #
@@ -72,12 +73,12 @@ print(lista5)'''
 #     print(liczba)
 #     liczba = liczba - 1
 
-'''X = list(range(0, 103, 3))
+X = list(range(0, 103, 3))
 print(f'x\ty')
 for x in X:
     y = 0.5 * x + 3
     # print(x, y)
-    print(f'{x}\t{y}')'''
+    print(f'{x}\t{y}')
 
 X = list(range(-30, 61, 1))
 print(f'x\ty')
@@ -96,8 +97,8 @@ lista3 = list(range(13, 131, 13))
 print(f'x\ty\tz')
 
 # sposob 1
-'''for x, y, z in zip(lista1, lista2, lista3):
-    print(f'{x}\t{y}\t{z}')'''
+for x, y, z in zip(lista1, lista2, lista3):
+    print(f'{x}\t{y}\t{z}')
 
 # sposob 2
 for i in range(10):
@@ -105,19 +106,42 @@ for i in range(10):
 
 lista = [7]
 
-'''for i in lista:
+for i in lista:
     liczba = float(input('podaj liczbe rzeczywista '))
     print(liczba)
     if liczba != 67:
-        lista.append(3)'''
+        lista.append(3)
 
-'''for i in lista:
+for i in lista:
     print(i)
-    lista.append(i + 1)'''
+    lista.append(i + 1)
 
 lista = [12, 45, 78, 101, 9, -5, 9, 7, 23]
-for i in range(len(lista)):
-    print(lista[1:8:2])
 
+for i in lista[1::2]:
+    print(i)'''
 
-
+#zadanie 17
+n = int(input('podaj ile bedzie liczb '))
+suma = 0
+max_liczba = -inf #minus nieskonczonosc
+min_liczba = inf #plus nieskonczonosc
+ile_mniej_3 = 0
+p = 0 #liczby nalezace do przedzialu (-2, 11]
+for x in range(n):
+    liczba = int(input('podaj liczbe '))
+    suma = suma + liczba
+    if liczba > max_liczba:
+        max_liczba = liczba
+    if liczba < min_liczba:
+        min_liczba = liczba
+    if liczba < 3:
+        ile_mniej_3 = ile_mniej_3 + 1
+    if liczba > -2 and liczba <= 11:
+        p = p + 1
+print(suma)
+print(suma / n)
+print(max_liczba)
+print(min_liczba)
+print(ile_mniej_3)
+print(p)
