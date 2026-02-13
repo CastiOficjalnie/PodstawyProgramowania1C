c69107deb84e6e66529e4e7491ab34534f631d74
kacper <kacper.jonski@gmail.com>
2026-02-13 19:18:28 +0100

    dom

--- listy.py
+++ listy.py
@@ -1,5 +1,5 @@
 # listy a napisy
-napis = "informatyka"
+'''napis = "informatyka"
 print(napis[2])
 
 lista = []
@@ -36,13 +36,13 @@ print(lista7)
 # 5. mnozenie listy przez liczbe
 lista9 = [0] * 1000
 print(lista9)
-'''lista10 = [[0] * 10] * 10 #dana ilosc list
+lista10 = [[0] * 10] * 10 #dana ilosc list
 lista10[0][0] = 5
-print(lista10)'''
+print(lista10)
 
-'''# sortowanie i odwracani listy
+# sortowanie i odwracani listy
 lista10 = {4, -1, 0, 5, 2, 9, 3}
-print(sorted(lista10))'''
+print(sorted(lista10))
 
 #wyrazenia listowe(listy skladane)
 lista11 = list(range(1, 11))
@@ -56,5 +56,63 @@ while 4 in lista12:
 print(lista12)
 #usuwanie elementu na bazie indeksu:
 lista13 = [5, 1, 8, 3, 9, 10]
-'''del.lista13[2] #usuwanie elementu o indeksie 2'''
+del.lista13[2]'''
+
+
+# zadanie 1.
+lista1 = [12, -9, 6, 2, 8, 1, 15, -7, 0, 1, 1, 2, 2, -7, 2, 1, -7, 2]
+lista2 = [['pies', 'wilk'], ['kot domowy', 'tygrys', 'lew'], 'kapibara', 'mrówka', ['rekin', 'sledz', 'pstrag']]
+'''print(lista2[1][2])
+print(list(lista2[3]))
+print(lista1[2::3])
+lista3 = lista2[1] * 3
+print(lista3)
+lista4 = lista2 + [lista3]
+print(lista4)'''
+
+'''lista3 = [9, 6, 16, -8, 7]
+print(lista1 + lista3)'''
+
+print(sorted(lista1))
+
+print(f'najmniejsza liczba - {min(lista1)}\nnajwieksza liczba - {max(lista1)}')
+
+'''del lista1[4]
+print(lista1)'''
+
+'''del lista1[4:9]
+print(lista1)'''
+
+'''while 2 in lista1:
+    lista1.remove(2)
+print(lista1)'''
+
+'''lista3 = [x ** 2for x in lista1]
+print(lista3)'''
+
+'''lista = [178, 192, 184, 182, 180, 179, 186, 190, 191, 191]
+
+lista_norm = [(x - min(lista)) / (max(lista) - min(lista)) for x in lista]
+print(lista_norm)'''
+
+'''lista = [123, 89, 5600, 432, 11, 45, 900, 12450, 1410, 390, 9999]
+
+cztery_cyfry = [[] for x in lista if x > 999 and x < 10000 else x]
+print(cztery_cyfry)'''
+
+inwokacja = 'LITWOOJCZYZNOMOJATYJESTESZJAKZDROWIEILECIETRZEBACENICTENTYLKOSIEDOWIEKTOCIESTRACILDZISPIEKNOSCTWACALEJOZDOBIEWIDZEIOPISUJEBOTESKNIEPOTOBIEPANNOSWIETACOJASNEJBRONISZCZESTOCHOWYIOSTREJSWIECISZBRAMIETYCOGRODZAMKOWYNOWOGRODZKIOCHRANIASZZJEGOWIERNMLUDEMJAKMNIEDZIECKODOZDROWIAPOWROCILASCUDYMGDYODPLACZACEJMATKIPODTWOJAOPIEKEOFIAROWANYMARTWAPODNIOSELMOJEPOWIEKEIZARAZMOGLEMPIESZODOTWYCZSWIATYNPROGUISTZAWROCONEZYCIEPODZIEKOWACBOGUTAKNASPOWROCISCUDYMNADOJCZYZNYLONOTYMCZASEMPRENOSMOJADUSZEUTESKNIONADOTYCHPAGORKOWLESNYCHDOTYCHLAKZIELONYCHSZEROKONADBLEKITNYMNIEMNEMROZCIAGNIONYCHDOTYCHPOLMALOWANYCHZBOZEMROZMAITEMWYZLACANYCHPSZENICAPOSREBRZANYCHZYTEMGDZIEBURSZTYNOWYSWIERZOPGRYKAJAKSNIEGBIALAGDZIEPANIENSKIMRUMIENCEMDZIECIELINAPALAAWSZYSTKOPRZEPASANEJAKBYWSTEGAMIEDZAZIELONANANIJZZADKACICHEGRUSZESIEDZA'
+
+lista = list(inwokacja)
+
+suma = 0
+
+
+for x in lista:
+    suma = suma + ord(x)
+
+print(suma)
+
+x = map(ord, list(inwokacja))
+
+print(x)
 
--- zmienne.py
+++ zmienne.py
@@ -34,3 +34,4 @@ print(type(slownik))
 
 
 
+
