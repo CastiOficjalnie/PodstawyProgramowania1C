5924ee6b9328dbdac25f47ad767ca869dc93de87
kacper <kacper@gmail.com>
2026-02-16 11:24:42 +0100

    lekcja

--- Sety.py
+++ Sety.py
@@ -4,3 +4,25 @@ print(zbior)
 #sprawdzanie ilosci tych samych elementow
 zbior2 = {'kot', 'pies', 'golab', 'kot', 'pies'}
 print(len(zbior2))
+
+slowa = [
+    'LETTER',
+    "BALLOON",
+    'SUCCESS',
+    'HAPPY',
+    'COFFEE',
+    'BOOKKEEPER',
+    'ASSESS',
+    'MISSISSIPPI',
+    'ADDRESS',
+    'TOOLBOX'
+]
+'''max_lrl = 0
+for x in slowa:
+    x_zbior = set(x)
+    lrl = len(x_zbior)
+    print(f'{x} {len(x_zbior)}')
+    if lrl > max_lrl:
+    print(x)'''
+
+
