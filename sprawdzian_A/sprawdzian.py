e432fde963e472c3730a966a35e332b5ae093fdb
kacper <kacper@gmail.com>
2026-04-08 10:27:30 +0200

    lekcja

--- sprawdzian_A/sprawdzian.py
+++ sprawdzian_A/sprawdzian.py
@@ -16,3 +16,21 @@ print(pola_mm)
 obwody = [2 * a + 2 * b for a, b in prostokaty]
 ile_roznych = len(set(obwody))
 print(ile_roznych)'''
+
+'''plik = open('przyklad.txt')
+dane = plik.readlines()
+
+for x in range(len(dane)):
+    dane[x] = dane[x].strip()
+
+for x in dane:
+    if int(x[::-1]) % 17 == 0:
+        print(x[::-1])'''
+plik = open('przyklad.txt')
+dane = plik.readlines()
+lista = []
+for x in dane:
+    ile = dane.count(x)
+    if ile == 2:
+        lista.append(x)
+print(len(lista))
---
+++ sprawdzian_B/sprawdzian.py
@@ -0,0 +1,15 @@
+plik = open('ruch.txt')
+dane = plik. readlines()
+for i in range(len(dane)):
+    dane[i] = dane[i].split()
+    dane[i] = list(map(float, dane[i]))
+print(dane)
+
+def t(i):
+    return (i - 1) / 100
+
+def Vsr(rv, rp, dt):
+    return [rk[0] - rp[0] / dt, rk[1] - rp[1] / dt]
+
+def szyb_sr(Vsr):
+    return (Vsr[0] +
